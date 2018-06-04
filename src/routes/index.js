6b0634b03e00604c6787f999114e9269239c696e
Hengli Yin <hengliy@jumei.com>
2018-06-04 11:38:08 +0800

    首页简单展示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 
 import { createStackNavigator } from 'react-navigation';
 
+import FirstScreen from '../screens/FirstPage'
 import HomeScreen from '../screens/Home'
 import AboutScreen from '../screens/About'
 
 
 export default createStackNavigator({
+  First: {
+    screen: FirstScreen
+  },
   Home: {
     screen: HomeScreen
   },
